b023288429a0b04776d16e48acfd548100e543e5
Aditya Bhattacharya <69953590+AdityaBhattacharya1@users.noreply.github.com>
2022-11-29 12:17:48 +0530

    asdf

--- server/ballin prototype.py
+++ server/ballin prototype.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import autoPopulateDB
+import tests.autoPopulateDB as autoPopulateDB
 
 username = "ptolemy"
 email = "bismillah@ixoti.com"
@@ -14,18 +14,13 @@ proqty = 100
 
 
 def servercheck():
-    ello = requests.get(f'http://localhost:8000/')
+    ello = requests.get(f"http://localhost:8000/")
     return ello.status_code == 200
 
 
 def register():
-    data = {
-        "username": username,
-        "email": email,
-        "password": password
-    }
-    regist = requests.post(
-        f'http://localhost:8000/registration/', json.dumps(data))
+    data = {"username": username, "email": email, "password": password}
+    regist = requests.post(f"http://localhost:8000/registration/", json.dumps(data))
     if regist.status_code == 200:
         return True
     else:
@@ -39,10 +34,9 @@ def propost():
         "price": proprice,
         "cover_image": proimage,
         "date_published": prodate,
-        "quantity": proqty
+        "quantity": proqty,
     }
-    product = requests.post(
-        f'http://localhost:8000/product/', json.dumps(data))
+    product = requests.post(f"http://localhost:8000/product/", json.dumps(data))
     if product.status_code == 201:
         return True
     else:
@@ -50,7 +44,7 @@ def propost():
 
 
 def proget():
-    product = requests.get(f'http://localhost:8000/product/')
+    product = requests.get(f"http://localhost:8000/product/")
     if product.status_code == 200:
         return True
     else:
@@ -59,15 +53,25 @@ def proget():
 
 def specpro():
     # autoPopulateDB.populateDB()
-    product = requests.get(f'http://localhost:8000/products/1')
-    if product.content == {"name": "1984", "genre": "fiction", "price": 20, "quantity": 10, "business_id": 1, } and product.status_code == 200:
+    product = requests.get(f"http://localhost:8000/products/1")
+    if (
+        product.content
+        == {
+            "name": "1984",
+            "genre": "fiction",
+            "price": 20,
+            "quantity": 10,
+            "business_id": 1,
+        }
+        and product.status_code == 200
+    ):
         return True
     else:
         return False
 
 
 def delpro():
-    product = requests.delete(f'http://localhost:8000/products/1')
+    product = requests.delete(f"http://localhost:8000/products/1")
     if product.status_code == 200:
         return True
     else:
@@ -81,10 +85,9 @@ def patchpro():
         "price": proprice,
         "cover_image": proimage,
         "date_published": prodate,
-        "quantity": proqty
+        "quantity": proqty,
     }
-    product = requests.patch(
-        f'http://localhost:8000/products/1', json.dumps(data))
+    product = requests.patch(f"http://localhost:8000/products/1", json.dumps(data))
     if product.status_code == 200:
         return True
     else:
@@ -92,7 +95,7 @@ def patchpro():
 
 
 def filter():
-    filter = requests.get(f'http://localhost:8000/filter/Gaming/')
+    filter = requests.get(f"http://localhost:8000/filter/Gaming/")
     if filter.status_code == 200:
         return True
     else:
@@ -100,14 +103,11 @@ def filter():
 
 
 def verify():
-    data = {
-        "username": username,
-        "password": password
-    }
-    token = requests.post(f'http://localhost:8000/token/', json.dumps(data))
+    data = {"username": username, "password": password}
+    token = requests.post(f"http://localhost:8000/token/", json.dumps(data))
     token = token.content
     print(token)
-    a = requests.get(f'http://localhost:8000/verification/', token)
+    a = requests.get(f"http://localhost:8000/verification/", token)
     if a.status_code == 200:
         return True
     else:
@@ -117,8 +117,7 @@ def verify():
 def addfile():
     file = open("static\images\d05391068b1f3cb2d72f.png", "rb")
     files = {"file": (file.name, file, "multi[art/form.data")}
-    addfile = requests.post(
-        url="http://localhost:8000/product/1", files=files)
+    addfile = requests.post(url="http://localhost:8000/product/1", files=files)
     if addfile.status_code == 201:
         return True
     else:
--- tests/ballin_ballin.py
+++ tests/ballin_ballin.py
@@ -1,120 +1,62 @@
 import requests
 import json
-import autoPopulateDB
-api='http://localhost:8000/'
-reg='registration/'
-pro='product/'
-proid='/'
-fil='filter/'
-tok='token/'
-ver='verification/'
-add='uploadfile/product/'
-username="Placeholder"
-email="yaba.doo@email.con"
-password="No"
-proname="FortniteBattlePass"
-procat="Gaming"
-proprice=1000
-proimage="battlepass.jpg"
-prodate="2022-08-06"
-proqty=100
+import os
+
+username = "badads"
+email = "ass7@gutkhamail.com"
+password = "Cbat1984"
+token = ""
+
+
 def servercheck():
-    ello=requests.get(api)
-    if ello==200:
-        return True
-    else:
-        return False
+    ello = requests.get(f"http://localhost:8000/")
+    return ello.status_code == 200
+
+
 def register():
-    data={
-        "username": username,
-        "email": email,
-        "password": password
-    }
-    register=requests.post(f'{api}{reg}', json.dumps(data))
-    if register==200:
-        return True
-    else:
-        return False
-def propost():
-    data={
-    "name": proname,
-    "genre": procat,
-    "price": proprice,
-    "cover_image": proimage,
-    "date_published": prodate,
-    "quantity": proqty
-    }
-    product=requests.post(f'{api}{pro}', json.dumps(data))
-    if product==201:
-        return True
-    else:
-        return False
-def proget():
-    product=requests.get(f'{api}{pro}')
-    if product==200:
-        return True
-    else:
-        return False
-def specpro():
-    autoPopulateDB.populateDB()
-    product=requests.get(f'{api}{pro}{proid}')
-    print(product)
-    if product.content == {"name": "1984", "genre": "fiction", "price": 20, "quantity": 10, "business_id": 1,} and product.status==200:
-        return True
-    else:
-        return False
-def delpro():
-    product=requests.delete(f'{api}{pro}{proid}')
-    if product==200:
-        return True
-    else:
-        return False
-def patchpro():
-    data={
-    "name": proname,
-    "genre": procat,
-    "price": proprice,
-    "cover_image": proimage,
-    "date_published": prodate,
-    "quantity": proqty
-    }
-    product=requests.patch(f'{api}{pro}{proid}', json.dumps(data))
-    if product==200:
-        return True
-    else:
-        return False
-def filter():
-    filter=requests.get(f'{api}{fil}{procat}/')
-    if filter==200:
-        return True
-    else:
-        return False
+    data = {"username": username, "email": email, "password": password}
+    regist = requests.post(f"http://localhost:8000/registration/", json.dumps(data))
+    return regist.status_code == 200
+
+
 def verify():
-    data={
-        "username": username,
-        "password": password
-    }
-    token=requests.post(f'{api}{tok}', json.dumps(data))
-    verify=requests.get(f'{api}{ver}', token)
-    if verify==200:
-        return True
-    else:
-        return False
-def addfile():
-    file=open("megaman.jpg", "rb")
-    files={"file": (f.name, f, "multi[art/form.data")}
-    addfile=requests.post(url="f'{api}'/create_file", files=files)
-    if addfile==201:
-        return True
-    else:
-        return False
-servercheck()
-register()
-verify()
-propost()
-proget()
-specpro()
-delpro()
-patchpro()
-filter()
-addfile()
+    global token
+    token = os.popen(
+        f'curl -X "POST" "http://127.0.0.1:8000/token" -N -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=&username={username}&password={password}"'
+    ).read()
+    token = str(token)
+    token = eval(token)
+    token = token["access_token"]
+    a = requests.get(f"http://localhost:8000/verification?token={token}")
+    return a.status_code == 200
+
+
+def proget():
+    product = requests.get("http://localhost:8000/products/")
+    return product.status_code == 200
+
+
+def filter():
+    filter1 = requests.get(f"http://localhost:8000/filter/fiction/")
+    filter2 = requests.get(f"http://localhost:8000/filter/Gaming/")
+    return [filter1.status_code, filter2.status_code] == [200, 404]
+
+
+def specpro():
+    # autoPopulateDB.populateDB()
+    product = requests.get("http://localhost:8000/products/1")
+    p = eval(product.content)
+    return (
+        p["data"]["product_details"]["name"] == "1984"
+        and p["data"]["product_details"]["price"] == 20
+        and product.status_code == 200
+    )
+
+
+print(servercheck())
+print(register())
+print(verify())
+print(filter())
+print(proget())
+print(specpro())
+print(token)
